3c244484f4ca2d9812cd2852d17630b84969bb8d
mikem1990 <112135737+mikem1990@users.noreply.github.com>
2023-03-27 19:00:26 -0400

    completed movie data and rating retrieval

--- movie recommendations.py
+++ movie recommendations.py
@@ -18,3 +18,25 @@ def get_related_titles(movies_list):
     for i in movies_list:
         recommended_list.extend(extract_movie_titles(get_movies_from_tastedive(i)))
     return list(set(recommended_list))
+
+# given movie, get movie data from omdb (year released, rating, runtime etc)
+def get_movie_data(movie):
+    parameters = {"t" : movie, "r" : "json"}
+    omdb_response = requests_with_caching.get("http://www.omdbapi.com/", params = parameters)
+    movie_data_dict = json.loads(omdb_response.text)
+    return movie_data_dict
+
+# get rotten tomatoes rating from movie data or 0 if no rating
+def get_movie_rating(movie_data_dict):
+    for i in movie_data_dict["Ratings"]:
+        if i["Source"] == "Rotten Tomatoes":
+            return int(i["Value"][:-1])         # i[Value] is str 'n%', [-1] to remove % sign and convert to int
+    return 0
+
+# given a list of movies, get recommended movies and their rotten tomato ratings, sorted non-ascending
+def get_sorted_recommendations(movies_list):
+    final_recommendations = []
+    for i in get_related_titles(movies_list):
+        rating_movie = (get_movie_rating(get_movie_data(i)), i)         # save rating and movie as tuple (rating, movie) for easy sorting                 
+        final_recommendations.append(rating_movie)
+    return [i[1] for i in sorted(final_recommendations, reverse = True)]            # return i[1] for movie title only
